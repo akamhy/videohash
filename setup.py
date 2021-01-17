fd54615c677a45d088899b4ca77cd4418bb5b0dc
Akash Mahanty <akamhy@yahoo.com>
2021-01-18 01:35:57 +0530

    + youtube_dl

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
         "video" "video diff",
         "compare videos",
     ],
-    install_requires=["ImageHash", "Pillow"],
+    install_requires=["ImageHash", "Pillow", "youtube_dl"],
     python_requires=">=3.4",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
