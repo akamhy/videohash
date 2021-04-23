f8fab01707e7db4845487b71f291b5c87013c957
Akash Mahanty <akamhy@yahoo.com>
2021-04-23 21:04:14 +0530

    Update __version__.py

--- videohash/__version__.py
+++ videohash/__version__.py
@@ -1,7 +1,7 @@
 __title__ = "videohash"
 __description__ = "Video Hashing Library"
 __url__ = "https://akamhy.github.io/videohash/"
-__version__ = "1.0.5"
+__version__ = "1.0.6"
 __author__ = "akamhy"
 __author_email__ = "akamhy@yahoo.com"
 __license__ = "MIT"
