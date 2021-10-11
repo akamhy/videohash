8759b6ad7fdabcdf4dd306df5f02825e2c6d7385
Akash Mahanty <akamhy@yahoo.com>
2021-10-11 20:54:45 +0530

    Update __version__.py

--- videohash/__version__.py
+++ videohash/__version__.py
@@ -1,7 +1,7 @@
 __title__ = "videohash"
 __description__ = "videohash - Perceptual Video Hashing package"
 __url__ = "https://akamhy.github.io/videohash/"
-__version__ = "2.0.0"
+__version__ = "2.0.1"
 __status__ = "production"
 __author__ = "Akash Mahanty"
 __author_email__ = "akamhy@yahoo.com"
