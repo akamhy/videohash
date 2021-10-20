671f095b762b7ead67d74a368ecbe9efcdbc4dac
Akash Mahanty <akamhy@yahoo.com>
2021-10-20 18:08:23 +0530

    a better ffmpeg downloader for tests on windows

--- assets/windows_ffmpeg_downloader_at_cwd.py
+++ assets/windows_ffmpeg_downloader_at_cwd.py
@@ -1,22 +1,36 @@
-from pyunpack import Archive
-import requests
 import os
+import tempfile
+import shutil
+from pyunpack import Archive
 from pathlib import Path
-from shutil import copyfile
+import requests
 
 
 cwd = os.getcwd()
-Path(cwd).mkdir(parents=True, exist_ok=True)
-ffmpeg_7z_path = os.path.join(cwd, "ffmpeg_full.7z")
+
+temp_folder = os.path.join(tempfile.mkdtemp(), ("temp_storage_dir%s" % os.path.sep))
+Path(temp_folder).mkdir(parents=True, exist_ok=True)
+
+ffmpeg_7z_path = os.path.join(temp_folder, "ffmpeg_full.7z")
 with open(ffmpeg_7z_path, "wb") as fd:
     fd.write(
         requests.get("https://www.gyan.dev/ffmpeg/builds/ffmpeg-git-full.7z").content
     )
-Archive(ffmpeg_7z_path).extractall(".")
-for file in os.listdir(cwd):
+Archive(ffmpeg_7z_path).extractall(temp_folder)
+for file in os.listdir(temp_folder):
     if "build" and "ffmpeg" in file:
-        copyfile(
-            os.path.join(os.path.join(cwd, (file + os.path.sep)), "bin", "ffmpeg.exe"),
+        shutil.copyfile(
+            os.path.join(
+                os.path.join(temp_folder, (file + os.path.sep)), "bin", "ffmpeg.exe"
+            ),
             os.path.join(cwd, "ffmpeg.exe"),
         )
+        shutil.copyfile(
+            os.path.join(
+                os.path.join(temp_folder, (file + os.path.sep)), "bin", "ffprobe.exe"
+            ),
+            os.path.join(cwd, "ffprobe.exe"),
+        )
         break
+
+shutil.rmtree(temp_folder, ignore_errors=True, onerror=None)
