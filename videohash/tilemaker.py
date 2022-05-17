64af6aa047f4e428c8ddf91ed630ef1f8db79185
Akash Mahanty <akamhy@yahoo.com>
2022-05-17 08:51:42 +0530

    create videohash/tilemaker.py and updated videohash/videohash.py
    There were some issues with only using whash for videos of samelength and kind of similar brighness levels.
    
    Exmaples in https://github.com/akamhy/videohash/issues/88
    
    To fix this issue we are making a image that is horizontally concatenation
    of all the frames used for the collage in whash
    
    Then we divide the horizontally concatenated into 64 equal tiles and
    find the dominant color of the tile.
    
    We have a deafult list of how we expect the dominant color to be, if they are
    same as our default list we set the bit to one else set it to zero.
    
    Finally take the bitwise XOR of the whash bit list and dominant color bitlist.

---
+++ videohash/tilemaker.py
@@ -0,0 +1,44 @@
+import sys
+
+from image_slicer import save_tiles, slice
+from PIL import Image
+
+from .utils import get_list_of_all_files_in_dir
+
+
+def concatenate_video_frames_horizontally(
+    frames_dir, horizontally_concatenated_image_path
+) -> None:
+    image_file_names = get_list_of_all_files_in_dir(frames_dir)
+    total_images = len(image_file_names)
+    first_image_filename = image_file_names[0]
+    with Image.open(first_image_filename) as first_frame_image_in_list:
+        width, height = first_frame_image_in_list.size
+
+    base_image = Image.new("RGB", (width * total_images, height))
+
+    x_offset = 0
+    for image_filename in image_file_names:
+        img = Image.open(image_filename)
+        base_image.paste(img, (x_offset, 0))
+        img.close()
+        x_offset += width
+        
+    base_image.save(horizontally_concatenated_image_path)
+
+
+def make_tile(frames_dir, horizontally_concatenated_image_path, tiles_dir) -> None:
+    concatenate_video_frames_horizontally(
+        frames_dir, horizontally_concatenated_image_path
+    )
+    tiles = list(
+        slice(
+            horizontally_concatenated_image_path,
+            number_tiles=64,
+            col=8,
+            row=8,
+            save=False,
+            DecompressionBombWarning=False,
+        )
+    )
+    save_tiles(tiles, prefix="tile", directory=tiles_dir, format="jpeg")
