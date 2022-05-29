e407bb0b26de625a846c2700072efccd9eb44e9a
Akash Mahanty <akamhy@yahoo.com>
2022-05-29 13:05:50 +0530

    v3.0.1

--- videohash/__version__.py
+++ videohash/__version__.py
@@ -6,7 +6,7 @@ __description__ = (
 )
 
 __url__ = "https://akamhy.github.io/videohash/"
-__version__ = "3.0.0"
+__version__ = "3.0.1"
 __status__ = "production"
 __author__ = "Akash Mahanty"
 __author_email__ = "akamhy@yahoo.com"
