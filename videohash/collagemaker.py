ea7635c1410ff6c593760648ae3d1dae911e77b5
Alberto Pasqualetto <39854348+albertopasqualetto@users.noreply.github.com>
2023-08-08 00:55:47 +0200

    Image.Resampling.LANCZOS instead of Image.ANTIALIAS deprecated in Pilllow 10

--- videohash/collagemaker.py
+++ videohash/collagemaker.py
@@ -180,7 +180,7 @@ class MakeCollage:
 
             # scale the opened frame images
             frame.thumbnail(
-                (scaled_frame_image_width, scaled_frame_image_height), Image.ANTIALIAS
+                (scaled_frame_image_width, scaled_frame_image_height), Image.Resampling.LANCZOS
             )
 
             # set the value of x to that of i's value.
